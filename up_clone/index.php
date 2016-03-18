1cbf97407c7cfd3355b448981b336fe2ef2b091f
asudhakar <asudhakar@live.in>
2016-03-18 12:15:46 +0530

    added an input box, if they click enter it will popup and clears the existing data

--- up_clone/index.php
+++ up_clone/index.php
@@ -17,9 +17,27 @@
 
 	<h1>Hello</h1>
 	<a href="view/login.php?action=logout">Logout</a>
-
+		<input type="textbox" id="textbox" name="user_question" placeholder="Please ask me anything!">
 	</div>
 
 </body>
 <script type="text/javascript" src="js/bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script type="text/javascript">
+		
+		$('#textbox').keypress(function(event){
+		
+			var keycode = (event.keyCode ? event.keyCode : event.which);
+			if(keycode == '13'){
+
+				alert('You pressed a "enter" key in textbox');	
+
+				$('#textbox').val("");
+				
+			}
+
+		});
+		
+	</script>
 </html>
+
