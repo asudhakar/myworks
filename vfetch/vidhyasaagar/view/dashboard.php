fb1d883e2a841f87f4dcb4ac28859445fc692ef1
sudhakar <asudhakar@live.in>
2016-03-29 15:21:57 +0530

    test

--- vfetch/vidhyasaagar/view/dashboard.php
+++ vfetch/vidhyasaagar/view/dashboard.php
@@ -28,11 +28,14 @@
             
   <table class="table">
     <tbody>
-      <?php foreach ($final_output as $class => $student_values) {
+      <?php
+      $i = 1; 
+      foreach ($final_output as $class => $student_values) {
       	foreach ($student_values as $number => $number_and_names) {
       		foreach ($number_and_names as $number => $names) {
       			foreach ($names as $key => $name) {
-      				echo "$name";
+      				echo $i."$name<br/><hr/>";
+      				$i++;
       			}
       		}
       	}
