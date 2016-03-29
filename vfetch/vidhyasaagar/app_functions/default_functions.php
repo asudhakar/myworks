157106ddaed72f2dffeaa442672b03f62fc76fa2
sudhakar <asudhakar@live.in>
2016-03-29 14:00:24 +0530

    file path modified

--- vfetch/vidhyasaagar/app_functions/default_functions.php
+++ vfetch/vidhyasaagar/app_functions/default_functions.php
@@ -5,7 +5,7 @@
 
     require('../SpreadsheetReader.php');
 
-    $Reader = new SpreadsheetReader('files/CUSTOMERDETAILS.xls');
+    $Reader = new SpreadsheetReader('../files/CUSTOMERDETAILS.xls');
     foreach ($Reader as $Row)
     {
     	$final_output[$Row[1]]['name'][$Row[3]][] = $Row[2];
