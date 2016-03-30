fc0cc44012f67b955d7812a652b8f49fccd4f1bf
sudhakar <asudhakar@live.in>
2016-03-30 12:30:09 +0530

    uploaded to selected directory

--- vfetch/vidhyasaagar/upload.php
+++ vfetch/vidhyasaagar/upload.php
@@ -7,6 +7,11 @@
 $target_dir = "files/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 echo $target_file;
+ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
 
 
 header("Location: view/dashboard.php?file_path=$target_file");
