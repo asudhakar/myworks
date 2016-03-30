910c63df237519f64b36ef5bcbf0ca30a4821845
sudhakar <asudhakar@live.in>
2016-03-30 11:00:03 +0530

    sql insertion

--- vfetch/vidhyasaagar/view/insert_into_db.php
+++ vfetch/vidhyasaagar/view/insert_into_db.php
@@ -13,3 +13,9 @@ $name_and_numbers = unserialize(base64_decode($name_and_numbers));
 echo "<pre>";
 print_r($name_and_numbers);
 echo "</pre>";
+
+
+foreach ($name_and_numbers as $key => $number) {
+	$sql = "INSERT INTO messageout SET ('MessageTo' => $number, 'MessageText' => $message)";
+	echo "$sql";
+}
