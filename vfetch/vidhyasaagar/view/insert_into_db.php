6406b2d1b22d074120c14862165d476ce665e985
sudhakar <asudhakar@live.in>
2016-03-30 10:53:51 +0530

    unserialize and base64 bug

--- vfetch/vidhyasaagar/view/insert_into_db.php
+++ vfetch/vidhyasaagar/view/insert_into_db.php
@@ -1,11 +1,15 @@
 <?php 
 
-
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
 
 
 $name_and_numbers = $_GET['name_and_number'];
 $message = $_GET['message'];
-$name_and_numbers = base64_decode(unserialize($name_and_numbers));
+$name_and_numbers = unserialize(base64_decode($name_and_numbers));
+
+
 echo "<pre>";
 print_r($name_and_numbers);
 echo "</pre>";
