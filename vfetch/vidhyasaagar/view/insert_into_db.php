25fd98084f6a06c739038b582bf548a8867de5cd
sudhakar <asudhakar@live.in>
2016-03-30 13:02:00 +0530

    final

--- vfetch/vidhyasaagar/view/insert_into_db.php
+++ vfetch/vidhyasaagar/view/insert_into_db.php
@@ -7,6 +7,17 @@ $message = $_POST['message'];
 $name_and_numbers = unserialize(base64_decode($name_and_numbers));
 $path = $_POST['path'];
 
+
+
+
+
+$link = db_connect();
+
+foreach ($name_and_numbers as $key => $number) {
+	$sql = "INSERT INTO `MessageOut` (`MessageTo` , `MessageText`) VALUES ('$number', '$message')";
+	executeQuery($sql, $link);
+	
+}
 if (!unlink($path))
   {
   echo ("Error deleting $path");
@@ -15,34 +26,23 @@ else
   {
   echo ("Deleted $path");
   }
+header('Location: ../index.php');
+
+	function db_connect(){
+		$connection = mysqli_connect('localhost', 'root', 'palaniM@67', 'up');
+		if (!$connection) {
+		    die("Connection failed: " . mysqli_connect_error());
+		    exit();
+		}
+		return $connection;
+	}
 
 
-
-// $link = db_connect();
-
-// foreach ($name_and_numbers as $key => $number) {
-// 	$sql = "INSERT INTO `MessageOut` (`MessageTo` , `MessageText`) VALUES ('$number', '$message')";
-// 	executeQuery($sql, $link);
-	
-// }
-
-// header('Location: ../index.php');
-
-// 	function db_connect(){
-// 		$connection = mysqli_connect('localhost', 'root', 'palaniM@67', 'up');
-// 		if (!$connection) {
-// 		    die("Connection failed: " . mysqli_connect_error());
-// 		    exit();
-// 		}
-// 		return $connection;
-// 	}
-
-
-// 	function executeQuery($query, $link){
-// 		if(!empty($link)){
-// 			return mysqli_query($link, $query);
-// 		}
-// 	}
+	function executeQuery($query, $link){
+		if(!empty($link)){
+			return mysqli_query($link, $query);
+		}
+	}
 
 
 
