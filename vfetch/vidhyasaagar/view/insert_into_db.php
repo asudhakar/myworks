31bcb1bb051d71e372325c90813c93127f09811c
sudhakar <asudhakar@live.in>
2016-04-03 15:16:14 +0530

    test

--- vfetch/vidhyasaagar/view/insert_into_db.php
+++ vfetch/vidhyasaagar/view/insert_into_db.php
@@ -11,7 +11,7 @@ $path = $_POST['path'];
 
 
 
-$link = db_connect();
+// $link = db_connect();
 
 foreach ($name_and_numbers as $key => $number) {
 	$sql = "INSERT INTO `MessageOut` (`MessageTo` , `MessageText`) VALUES ('$number', '$message')";
@@ -21,14 +21,14 @@ foreach ($name_and_numbers as $key => $number) {
 }
 // header('Location: ../index.php');
 
-	function db_connect(){
-		$connection = mysqli_connect('40.76.8.137', "vidyasaagar", "vidyasaagar", "vidyasaagar", 3306);
-		if (!$connection) {
-		    die("Connection failed: " . mysqli_connect_error());
-		    exit();
-		}
-		return $connection;
-	}
+	// function db_connect(){
+	// 	$connection = mysqli_connect('40.76.8.137', "vidyasaagar", "vidyasaagar", "vidyasaagar", 3306);
+	// 	if (!$connection) {
+	// 	    die("Connection failed: " . mysqli_connect_error());
+	// 	    exit();
+	// 	}
+	// 	return $connection;
+	// }
 
 
 
