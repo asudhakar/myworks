7a2fccc5702f304643f1bd70e16ae2c15b846d04
sudhakar <asudhakar@live.in>
2016-04-03 13:05:16 +0530

    UI design of files

--- vfetch/vidhyasaagar/index.php
+++ vfetch/vidhyasaagar/index.php
@@ -51,7 +51,7 @@
         // print_r($final_output);
 
         foreach ($final_output as $key => $value) {
-          echo '<a href="view/dashboard.php?file_path='.$value.'">'.$value.'</a><input type="submit" class="btn btn-warning" style="font-size: 13px;"value="delete">';
+          echo '<div><a href="view/dashboard.php?file_path='.$value.'">'.$value.'</a><form action="delete.php" method="get"><input type="submit" class="btn btn-danger" style="font-size: 13px;" name="id" value="'.$key.'"></form></div><br/>';
         }
         
 
