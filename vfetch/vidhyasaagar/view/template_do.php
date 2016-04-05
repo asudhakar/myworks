fb2f3999f9fd8e0ede3d8543d7428c6bbe687c84
sudhakar <asudhakar@live.in>
2016-04-05 16:30:33 +0530

    final page

--- vfetch/vidhyasaagar/view/template_do.php
+++ vfetch/vidhyasaagar/view/template_do.php
@@ -5,17 +5,15 @@ if(isset($_GET['delete'])){
 	$id = $_GET['delete'];
 	delete_template($id, $link);
 }else{
-	print_r($_POST);
 	$template_name = $_POST['template_name'];
 	$template_message = $_POST['template-message'];
 	insert_template($template_name, $template_message, $link);
 }
 
-// header("Location: manage_templates.php");
+header("Location: manage_templates.php");
 
 function insert_template($template_name, $template_message, $link){
 	$sql = "INSERT INTO `message_templates` (`template_name`, `template_message`) VALUES('$template_name','$template_message')";
-	echo "$sql";
 	executeQuery($sql, $link);
 }
 
